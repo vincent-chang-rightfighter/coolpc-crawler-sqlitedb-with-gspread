5051dcdbc5741075ac78b3d6b6690d8b6ff8663c
vincent <vincent870922@gmail.com>
2021-12-10 11:44:21 +0800

    run.sh add echo detail

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 while [ 1 ] ;do
-    echo "start run"
+    echo "Start run"
     sleep 5
+    echo "Start crawler"
     node index.js
     sleep 10
-    echo "crawler finish"
+    echo "Start Gspread Update"
     python3 SQLitetoGspreadUpdate.py
     sleep 5
-    echo "Gspread Update finish"
-    echo "sleep 40 second to re run"
+    echo "Sleep 40 second to re-run"
     sleep 40
 done
